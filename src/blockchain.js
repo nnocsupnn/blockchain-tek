4e80dcb77ef53748321ec47e7594ded237f8f81a
nincas <nincas21@gmail.com>
2021-11-14 23:21:27 +0800

    +

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,5 +1,5 @@
-const EC = require('elliptic').ec;
-const ec = new EC('secp256k1');
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
 const { Transaction } = require('./components/transaction')
 const { Block } = require('./components/block')
 
@@ -20,20 +20,26 @@ class Blockchain {
     }
 
     minePendingTransactions(miningRewardAddress) {
-        const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward)
+        const rewardTx = new Transaction(
+            null,
+            miningRewardAddress,
+            this.miningReward
+        )
         this.pendingTransactions.push(rewardTx)
 
-        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestblock().hash)
+        let block = new Block(
+            Date.now(),
+            this.pendingTransactions,
+            this.getLatestblock().hash
+        )
         block.mineBlock(this.difficulty)
 
         console.log('Block succesfully mined!')
         this.chain.push(block)
 
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ]
+        this.pendingTransactions = []
     }
-    
+
     addTransaction(transaction) {
         if (!transaction.fromAddress || !transaction.toAddress) {
             throw new Error('Transaction must include from and to address.')
@@ -43,12 +49,25 @@ class Blockchain {
             throw new Error('Cannot add invalid transaction to the chain.')
         }
 
+        if (transaction.amount <= 0) {
+            throw new Error('Transaction amount should be higher than 0')
+        }
+
+        // Making sure that the amount sent is not greater than existing balance
+        if (
+            this.getBalanceOfAddress(transaction.fromAddress) < transaction.amount
+        ) {
+            throw new Error('Not enough balance')
+        }
+
         this.pendingTransactions.push(transaction)
+
+        console.log('transaction added: %s', transaction)
     }
 
     getBalanceOfAddress(address) {
         let balance = 0
-        for (const  block of this.chain) {
+        for (const block of this.chain) {
             for (const trans of block.transactions) {
                 if (trans.fromAddress == address) {
                     balance -= trans.amount
@@ -60,10 +79,35 @@ class Blockchain {
             }
         }
 
+        console.log('getBalanceOfAdrees: %s', balance)
+
         return balance
     }
 
-    isChainValid () {
+    getAllTransactionsForWallet(address) {
+        const txs = []
+
+        for (const block of this.chain) {
+            for (const tx of block.transactions) {
+                if (tx.fromAddress === address || tx.toAddress === address) {
+                    txs.push(tx)
+                }
+            }
+        }
+
+        debug('get transactions for wallet count: %s', txs.length)
+        return txs
+    }
+
+    isChainValid() {
+        // Check if the Genesis block hasn't been tampered with by comparing
+        // the output of createGenesisBlock with the first block on our chain
+        const realGenesis = JSON.stringify(this.createGenesisBlock());
+
+        if (realGenesis !== JSON.stringify(this.chain[0])) {
+            return false;
+        }
+
         for (let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i]
             const previousBlock = this.chain[i - 1]
@@ -89,4 +133,3 @@ class Blockchain {
 }
 
 module.exports.Blockchain = Blockchain
-module.exports.Transaction = Transaction
--- src/components/block.js
+++ src/components/block.js
@@ -9,20 +9,18 @@ class Block {
         this.nonce = 0
     }
 
-    calculateHash () {
-        return crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).digest('hex')
-    }
+    calculateHash = () => crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).digest('hex')
 
-    mineBlock(difficulty) {
+    mineBlock = (difficulty) => {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.nonce++
             this.hash = this.calculateHash()
         }
-
+        
         console.log("Block mined: " + this.hash)
     }
 
-    hasValidTransaction() {
+    hasValidTransaction = () => {
         for (const tx of this.transactions) {
             if (!tx.isValid) return false
         }
